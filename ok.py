859f2476a5926d7dd00646852e8e7f03c3d753d8
Muh Alfani <79563472+defrieantoe1999@users.noreply.github.com>
2024-10-22 11:14:25 +0700

    Update ok.py

--- ok.py
+++ ok.py
@@ -2,6 +2,7 @@ from modules._config import TOKEN, bot
 from modules._helpers import load_modules
 from modules.custdl import file_server
 import asyncio
+from web import keep_alive
 
 bot.start(bot_token=TOKEN)
 
